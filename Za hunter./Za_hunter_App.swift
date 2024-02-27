90dcf968f2c42e370974a1217e7e527f7a520190
justin rebbeck <jrebbeck2bsd220.org>
2024-02-27 09:28:33 -0600

    Initial Commit

---
+++ Za hunter./Za_hunter_App.swift
@@ -0,0 +1,17 @@
+//
+//  Za_hunter_App.swift
+//  Za hunter.
+//
+//  Created by Justin Rebbeck on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Za_hunter_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
